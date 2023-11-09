58117fb29e42def0464287acb13d9bc6499ea139
lamnaourm <mohammed.lamnaour@gmail.com>
2023-11-09 12:40:08 +0100

    solution

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -5,11 +5,10 @@ import { useSelector } from 'react-redux'
 export default function Show() {
 
     const counter = useSelector(state => state.counter)
-    
+
     return (
         <div>
             <h1>{counter}</h1>
-            <h1>{id}</h1>
         </div>
     )
 }
